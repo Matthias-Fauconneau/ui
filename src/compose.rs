4b9c777e59a1a0ea31980485ad4c304e2d2619fa
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2020-01-01 09:39:44 +0100

    Op op

--- src/compose.rs
+++ src/compose.rs
@@ -1,9 +1,27 @@
 pub trait FnRef<Args> { type Output; fn call(&self, args: Args) -> Self::Output; } // impl Fn/Mut/Once with a simpler FnRef trait
-#[macro_export] macro_rules! FnRef { ($T:ident<$($args:ident),+>) => (
-impl<$($args),+, Args> FnOnce<Args> for $T<$($args),+> where Self:$crate::compose::FnRef<Args> { type Output=<Self as $crate::compose::FnRef<Args>>::Output;
-                                                                                                                               extern "rust-call" fn call_once(self, args: Args) -> Self::Output { <Self as Fn<Args>>::call(&self, args) } }
-impl<$($args),+, Args> FnMut<Args> for $T<$($args),+> where Self:Fn<Args> { extern "rust-call" fn call_mut(&mut self, args: Args) -> Self::Output { <Self as Fn<Args>>::call(&self, args) } }
-impl<$($args),+, Args> Fn<Args> for $T<$($args),+> where Self:$crate::compose::FnRef<Args> { extern "rust-call" fn call(&self, args: Args) -> Self::Output { <Self as $crate::compose::FnRef<Args>>::call(&self, args) } }
+#[macro_export] macro_rules! FnRef { ($T:ident<$($Params:ident),+>) => (
+    impl<$($Params),+, Args> FnOnce<Args> for $T<$($Params),+> where Self:$crate::compose::FnRef<Args> { 
+        type Output=<Self as $crate::compose::FnRef<Args>>::Output;
+        extern "rust-call" fn call_once(self, args: Args) -> Self::Output { <Self as Fn<Args>>::call(&self, args) } 
+    }
+    impl<$($Params),+, Args> FnMut<Args> for $T<$($Params),+> where Self:Fn<Args> {
+        extern "rust-call" fn call_mut(&mut self, args: Args) -> Self::Output { <Self as Fn<Args>>::call(&self, args) } 
+    }
+    impl<$($Params),+, Args> Fn<Args> for $T<$($Params),+> where Self:$crate::compose::FnRef<Args> {
+        extern "rust-call" fn call(&self, args: Args) -> Self::Output { <Self as $crate::compose::FnRef<Args>>::call(&self, args) } 
+    }
+)}
+
+#[macro_export] macro_rules! Op { ($Op:ident) => (
+pub struct $Op<A,B>(A,B);
+impl<Args:Copy,A:Fn<Args>,B:Fn<Args>> $crate::compose::FnRef<Args> for $Op<A,B>
+where Self:$crate::compose::StaticMethod<(<A as FnOnce<Args>>::Output,<B as FnOnce<Args>>::Output)> {
+    type Output=<Self as $crate::compose::StaticMethod<(<A as FnOnce<Args>>::Output,<B as FnOnce<Args>>::Output)>>::Output;
+    fn call(&self, args: Args) -> Self::Output {
+        <Self as $crate::compose::StaticMethod<(<A as FnOnce<Args>>::Output,<B as FnOnce<Args>>::Output)>>::call((self.0.call(args), self.1.call(args)))
+    }
+}
+FnRef!($Op<A,B>);
 )}
 
 pub trait StaticMethod<Args> {
@@ -14,27 +32,39 @@ pub trait StaticMethod<Args> {
     impl<A:std::ops::$Op<B>,B,C,F> $crate::compose::StaticMethod<(A,B)> for $Op<C, F> { type Output=A::Output; fn call((a,b):(A,B)) -> Self::Output { a.$op(b) } }
 )}
 
-#[macro_export] macro_rules! Op { ($Op:ident) => (
-pub struct $Op<A,B>(A,B);
-impl<Args:Copy,A:Fn<Args>,B:Fn<Args>> $crate::compose::FnRef<Args> for $Op<A,B> where Self:$crate::compose::StaticMethod<(<A as FnOnce<Args>>::Output,<B as FnOnce<Args>>::Output)> {
-    type Output=<Self as $crate::compose::StaticMethod<(<A as FnOnce<Args>>::Output,<B as FnOnce<Args>>::Output)>>::Output;
-    fn call(&self, args: Args) -> Self::Output {
-        <Self as $crate::compose::StaticMethod<(<A as FnOnce<Args>>::Output,<B as FnOnce<Args>>::Output)>>::call((self.0.call(args), self.1.call(args)))
+#[macro_export] macro_rules! Fn_op_Fn { ($Op:ident $op:ident $Args:ty) => (
+    impl<A:Fn<$Args>,B:Fn<$Args>> std::ops::$Op<Closure<B>> for Closure<A> { 
+        type Output = $Op<A,B>;
+        fn $op(self, b:Closure<B>) -> Self::Output { $Op(self.0, b.0) } 
     }
-}
-FnRef!($Op<A,B>);
-)}
-
-#[macro_export] macro_rules! Closure_op_Fn { ($Op:ident $op:ident $Args:ty) => (
-    impl<A:Fn<$Args>,B:Fn<$Args>> std::ops::$Op<B> for Closure<A> { type Output = $Op<A,B>; fn $op(self, b:B) -> Self::Output { $Op(self.0, b) } }
+    impl<A:Fn<$Args>,B:Fn<$Args>> std::ops::$Op<B> for Closure<A> { 
+        type Output = $Op<A,B>;
+        fn $op(self, b:B) -> Self::Output { $Op(self.0, b) } 
+    }
 )}
 
 pub struct Uniform<T>(pub T);
 impl<T:Copy,Args> FnRef<Args> for Uniform<T> { type Output=T; fn call(&self, _: Args) -> Self::Output { self.0 } }
 FnRef!(Uniform<T>);
 
-#[macro_export] macro_rules! T_op_Closure { ($Op:ident $op:ident $T:ty) => (
-    impl<F> std::ops::$Op<Closure<F>> for $T { type Output = $Op<$crate::compose::Uniform<$T>,F>; fn $op(self, b:Closure<F>) -> Self::Output { $Op($crate::compose::Uniform(self), b.0) } }
+#[macro_export] macro_rules! Uniform_op_Closure { 
+    ($Op:ident $op:ident $Args:ty, $A:ty) => (
+        impl<B:Fn<$Args>> std::ops::$Op<Closure<B>> for $A {
+            type Output = $Op<$crate::compose::Uniform<$A>,B>; 
+            fn $op(self, b:Closure<B>) -> Self::Output {
+                $Op($crate::compose::Uniform(self), b.0)
+            }
+        }
+    )
+}
+
+#[macro_export] macro_rules! op { ($Op:ident $op:ident $Args:ty, $A:ty) => ( 
+    Op!($Op);
+    Op_static_method!($Op $op); 
+    Fn_op_Fn!($Op $op $Args);
+    Uniform_op_Closure!($Op $op $Args, $A); 
 )}
 
-#[macro_export] macro_rules! op { ($Op:ident $op:ident $Args:ty, $T:ty) => ( Op!($Op); Op_static_method!($Op $op); Closure_op_Fn!($Op $op $Args); T_op_Closure!($Op $op $T); ) }
+#[macro_export] macro_rules! Op_op { ( $A:ident $Op:ident ) => (
+    impl<AA,AB,B> std::ops::$Op<Closure<B>> for $A<AA,AB> { type Output=$Op<Self,B>; fn add(self, b: Closure<B>) -> Self::Output { $Op(self, b.0) } }  
+)}
--- tests/compose.rs
+++ tests/compose.rs
@@ -1,9 +1,12 @@
 #![feature(unboxed_closures,fn_traits)]
+#![allow(incomplete_features)]#![feature(impl_trait_in_bindings)]
 #[macro_use] extern crate framework;
 #[derive(Clone,Copy)] pub struct Closure<F>(pub F); // local type for T op Fn
-op!(Mul mul ((i32,i32),), f32);
-op!(Sub sub ((i32,i32),), f32);
+trait LocalFn<T> : Fn<T> {}
+op!(Add add (i32,i32), f32);
+op!(Sub sub (i32,i32), f32); Op_op!(Sub Add);
+op!(Mul mul (i32,i32), f32);
 #[test] fn test() {
-    let x = Closure(|(_,_):(i32,i32)| 1.);
-    assert_eq!((x-1.*x)((0,0)), 0.);
+    let x = Closure(|_:i32,_:i32| 1.);
+    assert_eq!((x-1.*x+x)(0,0), 1.);
 }
