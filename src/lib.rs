ba01cfdf82b589a244b6ab22f78003dd77a6ad3b
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2020-10-27 15:21:38 +0100

    move plot to ui

--- src/lib.rs
+++ src/lib.rs
@@ -15,6 +15,7 @@
 #[cfg(feature="text")] pub mod text; //cfg_if! { if #[cfg(feature="text")] { pub mod text; pub use text::{Text, default_font, default_style}; }}
 #[cfg(feature="edit")] pub mod edit; //cfg_if! { if #[cfg(feature="text-edit")] { pub mod edit; pub use edit::TextEdit; }}
 #[cfg(feature="graphic")] pub mod graphic; //cfg_if! { if #[cfg(feature="graphic")] { pub mod graphic; pub use graphic::{Glyph, Graphic}; }}
+#[cfg(feature="plot")] pub mod plot; //cfg_if! { if #[cfg(feature="graphic")] { pub mod graphic; pub use graphic::{Glyph, Graphic}; }}
 
 pub fn time<T>(id: &str, task: impl FnOnce() -> T) -> T {
 	let time = std::time::Instant::now();
