5df512cc81fba388f4ac6bdd3c6c742588bf3eb9
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2025-01-18 20:04:40 +0100

    multiple images

--- examples/view/src/main.rs
+++ examples/view/src/main.rs
@@ -1,18 +1,19 @@
 #![feature(slice_from_ptr_range)] // shader
-use {ui::{Result, size, int2, Widget, vulkan, shader}, ::image::{Image, f32, sRGB8_OETF12, oetf8_12, rgb, rgba8}};
-use vulkan::{Context, Commands, Arc, ImageView, from_iter, BufferUsage, Image as GPUImage, default, ImageCreateInfo, ImageType, Format, ImageUsage,
-	CopyBufferToImageInfo, Sampler, SamplerCreateInfo, Filter, WriteDescriptorSet};
+#![feature(iterator_try_collect)]
+use {ui::{Result, size, int2, Widget, EventContext, Event::{self, Key}, vulkan, shader}, ::image::{Image, f32, sRGB8_OETF12, oetf8_12, rgb, rgba8}};
+use vulkan::{Context, Commands, Arc, ImageView, from_iter, BufferUsage, Image as GPUImage, default, ImageCreateInfo, ImageType, Format, ImageUsage, CopyBufferToImageInfo, Sampler, SamplerCreateInfo, Filter, WriteDescriptorSet};
 shader!{view}
 
 struct App {
 	pass: view::Pass,
-	image: Arc<GPUImage>
+	images: Vec<Arc<GPUImage>>,
+	index: usize,
 }
 impl App {
 	fn new(context@Context{memory_allocator, ..}: &Context, commands: &mut Commands) -> Result<Self> { Ok(Self{
 		pass: view::Pass::new(context, false)?,
-		image: {
-			let Image{size, data, ..} = f32(std::env::args().skip(1).next().unwrap())?;
+		images: std::env::args().skip(1).map(|path| -> Result<_> {
+			let Image{size, data, ..} = f32(path)?;
 			fn minmax(values: &[f32]) -> [f32; 2] {
 				let [mut min, mut max] = [f32::INFINITY, -f32::INFINITY];
 				for &value in values { if value > f32::MIN && value < min { min = value; } if value > max { max = value; } }
@@ -33,20 +34,29 @@ impl App {
 			let oetf = &sRGB8_OETF12;
 			let buffer = from_iter(context, BufferUsage::TRANSFER_SRC, data.into_iter().map(|v| rgba8::from(rgb::from(oetf8_12(oetf, ((v-min)/(max-min)).clamp(0., 1.))))))?;
 			commands.copy_buffer_to_image(CopyBufferToImageInfo::buffer_image(buffer, image.clone()))?;
-			image
-		},
+			Ok(image)
+		}).try_collect()?,
+		index: 0,
 	})}
 }
 impl Widget for App { 
 fn paint(&mut self, context@Context{device, ..}: &Context, commands: &mut Commands, target: Arc<ImageView>, _: size, _: int2) -> Result<()> {
+	let Self{pass, images, index} = self;
 	let sampler = Sampler::new(device.clone(), SamplerCreateInfo{mag_filter: Filter::Linear, min_filter: Filter::Linear, ..default()})?;
-	self.pass.begin_rendering(context, commands, target.clone(), None, true, &view::Uniforms::empty(), &[
-		WriteDescriptorSet::image_view(0, ImageView::new_default(self.image.clone())?),
+	pass.begin_rendering(context, commands, target.clone(), None, true, &view::Uniforms::empty(), &[
+		WriteDescriptorSet::image_view(0, ImageView::new_default(images[*index].clone())?),
 		WriteDescriptorSet::sampler(1, sampler.clone()),
 	])?;
 	unsafe{commands.draw(3, 1, 0, 0)}?;
 	commands.end_rendering()?;
 	Ok(())
+}
+fn event(&mut self, _size: size, _: &mut EventContext, event: &Event) -> Result<bool> {
+	Ok(match event {
+		Key('←') => { self.index = (self.index+self.images.len()-1)%self.images.len(); true },
+		Key('→') => { self.index = (self.index+1)%self.images.len(); true },
+		_ => false,
+	})
 }
 }
 
--- src/app.rs
+++ src/app.rs
@@ -272,8 +272,12 @@ pub fn run(title: &str, app: Box<dyn std::ops::FnOnce(&Context, &mut Commands) -
 						wm_base.pong(serial);
 					}
 					else if id == keyboard.id && opcode == keyboard::key {
-						let [_serial,UInt(_key_time),UInt(key),UInt(_state)] = server.args({use Type::*; [UInt,UInt,UInt,UInt]}) else {unreachable!()};
+						let [_serial,UInt(_key_time),UInt(key),UInt(state)] = server.args({use Type::*; [UInt,UInt,UInt,UInt]}) else {unreachable!()};
 						if key == 1 { return Ok(()); }
+						if state > 0 {
+							#[allow(non_upper_case_globals)] const usb_hid_usage_table: [char; 126] = ['\0','⎋','1','2','3','4','5','6','7','8','9','0','-','=','⌫','\t','q','w','e','r','t','y','u','i','o','p','{','}','\n','⌃','a','s','d','f','g','h','j','k','l',';','\'','`','⇧','\\','z','x','c','v','b','n','m',',','.','/','⇧','\0','⎇',' ','⇪','\u{F701}','\u{F702}','\u{F703}','\u{F704}','\u{F705}','\u{F706}','\u{F707}','\u{F708}','\u{F709}','\u{F70A}','\0','\0','\0','\0','\0','\0','\0','\0','\0','\0','\0','\0','\0','\0','\0','\0','\0','\0','\0','\0','\u{F70B}','\u{F70C}','\0','\0','\0','\0','\0','\0','\0','\0','⎙','⎄',' ','⇤','↑','⇞','←','→','⇥','↓','⇟','⎀','⌦','\u{F701}','🔇','🕩','🕪','⏻','=','±','⏯','🔎',',','\0','\0','¥','⌘'];
+							if app.event(size, &mut EventContext{modifiers_state}, &Event::Key(usb_hid_usage_table[key as usize]))? { need_paint = true; }
+						}
 					}
 					else if window.callback.as_ref().is_some_and(|callback| id == callback.id) && opcode == callback::done {
 						let [UInt(_timestamp_ms)] = server.args({use Type::*; [UInt]}) else {unreachable!()};
