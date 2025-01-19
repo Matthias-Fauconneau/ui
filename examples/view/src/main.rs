efc9371563c4852da5ac6474522f4e1000455d3a
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2025-01-19 01:18:37 +0100

    update, rgb

--- examples/view/src/main.rs
+++ examples/view/src/main.rs
@@ -1,6 +1,6 @@
 #![feature(slice_from_ptr_range)] // shader
 #![feature(iterator_try_collect)]
-use {ui::{Result, time, size, int2, Widget, EventContext, Event::{self, Key}, vulkan, shader}, ::image::{Image, u8, f32, sRGB8_OETF12, oetf8_12, rgb, rgba8}};
+use {ui::{Result, time, size, int2, Widget, EventContext, Event::{self, Key}, vulkan, shader}, ::image::{Image, rgb, rgb8, rgba8, f32, sRGB8_OETF12, oetf8_12}};
 use vulkan::{Context, Commands, Arc, ImageView, Image as GPUImage, image, WriteDescriptorSet, linear};
 shader!{view}
 
@@ -10,7 +10,7 @@ struct App {
 	index: usize,
 }
 impl App {
-	fn new(context@Context{memory_allocator, ..}: &Context, commands: &mut Commands) -> Result<Self> { Ok(Self{
+	fn new(context: &Context, commands: &mut Commands) -> Result<Self> { Ok(Self{
 		pass: view::Pass::new(context, false)?,
 		images: std::env::args().skip(1).map(|ref path| image(context, commands, if let Ok(Image{size, data, ..}) = f32(path) {
 				fn minmax(values: &[f32]) -> [f32; 2] {
@@ -21,17 +21,17 @@ impl App {
 				let [min, max] = minmax(&data);
 				let oetf = &sRGB8_OETF12;
 				Image::new(size, data.into_iter().map(|v| rgba8::from(rgb::from(oetf8_12(oetf, ((v-min)/(max-min)).clamp(0., 1.))))).collect())
-			} else { time!(u8(path)).map(|v| rgba8::from(rgb::from(v))) })
+			} else { time!(rgb8(path)).map(|v| rgba8::from(v)) }.as_ref())
 		).try_collect()?,
 		index: 0,
 	})}
 }
 impl Widget for App { 
-fn paint(&mut self, context@Context{device, ..}: &Context, commands: &mut Commands, target: Arc<ImageView>, _: size, _: int2) -> Result<()> {
+fn paint(&mut self, context: &Context, commands: &mut Commands, target: Arc<ImageView>, _: size, _: int2) -> Result<()> {
 	let Self{pass, images, index} = self;
 	pass.begin_rendering(context, commands, target.clone(), None, true, &view::Uniforms::empty(), &[
 		WriteDescriptorSet::image_view(0, ImageView::new_default(images[*index].clone())?),
-		WriteDescriptorSet::sampler(1, linear()),
+		WriteDescriptorSet::sampler(1, linear(context)),
 	])?;
 	unsafe{commands.draw(3, 1, 0, 0)}?;
 	commands.end_rendering()?;
